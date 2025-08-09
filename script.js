ca54e9a2eabe8d426e306a436c061fdf8141f231
malikraheelrugh <raheelrugh677@gmail.com>
2025-08-09 08:42:17 +0500

    updated validation using regex

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
 userData = [
-  { label: "name", type: "text", isRequired: true },
-  { label: "user", type: "text", isRequired: false },
-  { label: "phone", type: "number", isRequired: false },
-  { label: "email", type: "email", isRequired: true },
-  { label: "password", type: "password", isRequired: true },
+  {
+    label: "name",
+    type: "text",
+    isRequired: true,
+    validationtype: /^[a-zA-Z]/,
+  },
+  {
+    label: "user",
+    type: "text",
+    isRequired: true,
+    validationtype: /^[a-zA-Z]/,
+  },
+  {
+    label: "phone",
+    type: "phone",
+    isRequired: true,
+    validationtype: /^((\+92|0092|0)?3\d{2}-?\d{7}|03\d{2}-?\d{7})$/,
+  },
+  {
+    label: "email",
+    type: "email",
+    isRequired: true,
+    validationtype: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  },
+  {
+    label: "password",
+    type: "password",
+    isRequired: true,
+    validationtype: /^.{8}$/,
+  },
+  {
+    label: "gender",
+    type: "radio",
+    isRequired: false,
+    validationtype: "string",
+    Options: ["Male", "female", "unknown"],
+  },
+  {
+    label: "checkbox",
+    type: "checkbox",
+    isRequired: false,
+    validationtype: "",
+  },
+  {
+    label: "websiteURL",
+    type: "text",
+    isRequired: false,
+    validationtype:
+      "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\. ]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$",
+  },
 ];
 //make form and append to body
 let form = document.createElement("form");
@@ -24,9 +69,9 @@ userData.forEach((item) => {
   label.textContent = item.label;
   // Add star only if isRequired is true
   if (item.isRequired) {
-    const star = document.createElement("span");
+    const star = document.createElement("small");
     star.setAttribute("class", "star");
-    star.textContent = " *";
+    star.textContent = " * ";
     label.appendChild(star);
   }
   let input = document.createElement("input");
@@ -36,17 +81,60 @@ userData.forEach((item) => {
   maindiv.append(label);
   maindiv.append(input);
   if (item.isRequired) {
+    let span = document.createElement("p");
+    span.innerHTML = `${item.label} is required`;
+    span.setAttribute("id", `span${item.label}`);
+    maindiv.appendChild(span);
+  }
+  if (item.type == "radio") {
+    input.remove();
+    let gender = document.createElement("div");
+    item.Options.forEach((option) => {
+      const radioInput = document.createElement("input");
+      radioInput.type = "radio";
+      radioInput.name = item.label;
+      radioInput.value = option;
+      radioInput.id = `${item.label}`;
+      const radioLabel = document.createElement("label");
+      radioLabel.setAttribute("for", radioInput.id);
+      radioLabel.style.fontSize = "14px";
+      radioLabel.style.color = "black";
+      let error = document.createElement("small");
+      error.setAttribute("class", item.label);
+      maindiv.appendChild(error);
+
+      radioLabel.textContent = option;
+      gender.appendChild(radioInput);
+      gender.appendChild(radioLabel);
+    });
+    maindiv.appendChild(gender);
+  }
+  if (item.type == "checkbox") {
+    let checkboxDiv = document.createElement("div");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", item.label);
+    let label = document.createElement("label");
+    label.setAttribute("for", item.label);
+    label.innerHTML = "I agree to the terms and conditions<br>";
+    label.style.fontSize = "17px";
+    label.style.color = "black";
     let error = document.createElement("small");
     error.setAttribute("class", item.label);
-    error.innerHTML = `please fill in the ${item.label}`;
+    error.innerHTML = "checkbox must be checked";
+    maindiv.appendChild(checkboxDiv);
+    checkboxDiv.appendChild(checkbox);
+    checkboxDiv.appendChild(label);
+    checkboxDiv.appendChild(error);
+  }
+  if (item.type != "checkbox" && item.type != "radio") {
+    let error = document.createElement("small");
+    error.setAttribute("class", item.label);
+    error.innerHTML = `please enter a valid ${item.label}`;
     error.style.color = "red";
+    error.style.visibility = "hidden";
     maindiv.appendChild(error);
   }
 });
-//create checkbox div
-let checkboxDiv = document.createElement("div");
-checkboxDiv.style.marginTop = "1.5rem";
-form.append(checkboxDiv);
 //create submit btn
 let submitBtn = document.createElement("button");
 submitBtn.innerHTML = "Submit";
@@ -66,30 +154,40 @@ const myName = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const inputs = document.querySelectorAll("input");
+const radioInputs = document.querySelectorAll("#gender");
 
-myName.addEventListener("input", () => {
-  document.querySelector(".name").style.visibility = "hidden";
+let radio = Array.from(radioInputs);
+radio.forEach((radio) => {
+  radio.addEventListener("change", (e) => {
+    document.querySelector(".gender").style.visibility = "hidden";
+  });
 });
-email.addEventListener("input", () => {
-  document.querySelector(".email").style.visibility = "hidden";
-});
-password.addEventListener("input", () => {
-  document.querySelector(".password").style.visibility = "hidden";
-});
+console.log(submitBtn);
 
-//take output from input fields
-
+//add event listener to submit button
 btn.addEventListener("click", () => {
+  console.log(validationStatus);
+  refreshPage();
+  // required();
+  validate();
   error();
-  validateForm();
 });
-let users = [];
-let currentIndex = null;
-function validateForm() {
-  if (myName.value != "" && email.value != "" && password.value != "") {
+function validate() {
+  if (
+    validationStatus.name == true &&
+    validationStatus.user == true &&
+    validationStatus.phone == true &&
+    validationStatus.email == true &&
+    validationStatus.password == true
+  ) {
+    alert("form submitted successfully");
     submitCall();
+  } else {
   }
 }
+let users = [];
+let currentIndex = null;
+
 function submitCall() {
   let user = {};
 
@@ -152,3 +250,79 @@ function error() {
     item.style.visibility = "visible";
   });
 }
+//strong validation using validatetype
+let validationStatus = {};
+function validatetype() {
+  userData.forEach((item) => {
+    let isValid = item.validationtype;
+    let input = document.getElementById(item.label);
+
+    input.addEventListener("change", () => {
+      let value = input.value;
+      if (item.type != "radio" && item.type != "checkbox") {
+        if (!value.match(isValid)) {
+          document.querySelector(
+            `.${item.label}`
+          ).innerHTML = `please enter a valid ${item.label}`;
+          document.querySelector(`.${item.label}`).style.color = "red";
+
+          document.querySelector(`.${item.label}`).style.visibility = "visible";
+
+          validationStatus[item.label] = false;
+        } else {
+          document.querySelector(
+            `.${item.label}`
+          ).textContent = `your ${item.label} is valid`;
+          document.querySelector(`.${item.label}`).style.color = "green";
+          document.querySelector(`#${item.label}`).style.border =
+            "2px solid green";
+          validationStatus[item.label] = true;
+        }
+      }
+    });
+  });
+}
+validatetype();
+
+document.getElementById("checkbox").addEventListener("change", (e) => {
+  if (e.target.checked) {
+    document.querySelector(".checkbox").style.visibility = "hidden";
+  } else {
+    document.querySelector(".checkbox").style.visibility = "visible";
+  }
+});
+function refreshPage() {
+  userData.forEach((item) => {
+    document.querySelector(
+      `.${item.label}`
+    ).innerHTML = `please enter a valid ${item.label}`;
+    document.querySelector(`.${item.label}`).style.color = "red";
+    document.querySelector(`.${item.label}`).style.visibility = "hidden";
+    document.querySelector(`#${item.label}`).style.border = "2px solid red";
+  });
+  document.querySelector(".password").innerHTML =
+    "Password must be at least 8 characters long ";
+  document.querySelector(".gender").innerHTML = "Please select gender";
+  document.querySelector(".checkbox").innerHTML = "checkbox must be checked";
+}
+document.querySelector(".password").innerHTML =
+  "Password must be at least 8 characters long ";
+document.querySelector(".gender").innerHTML = "Please select gender";
+document.querySelector(".checkbox").innerHTML = "checkbox must be checked";
+
+// refreshPage();
+function required() {
+  userData.forEach((item) => {
+    const input = document.getElementById(`${item.label}`);
+
+    if (item.isRequired) {
+      // if (input.type != "checkbox" && input.type != "radio") {
+      input.addEventListener("input", (e) => {
+        document.querySelector(`.${item.label}`).style.visibility = "visible";
+        document.querySelector(`#span${item.label}`).innerHTML = "";
+      });
+      // }
+    }
+  });
+}
+required();
