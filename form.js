8f317fa5fae80e1bea382ddcd053ffcb5756c100
malikraheelrugh <raheelrugh677@gmail.com>
2025-08-24 00:21:27 +0500

    updated

--- form.js
+++ form.js
@@ -54,6 +54,9 @@ userData = [
 let form = document.createElement("form");
 form.setAttribute("action", "/");
 document.body.append(form);
+let localstore = document.createElement("h4");
+localstore.setAttribute("id", "local-storage");
+document.body.append(localstore);
 //heading
 let h1 = document.createElement("h1");
 h1.innerHTML = "Dynamic form";
@@ -146,12 +149,21 @@ form.append(submitBtn);
 const output1 = document.createElement("div");
 output1.setAttribute("id", "output");
 document.body.append(output1);
+// Show last saved user data on page load
+// const savedUsers = JSON.parse(localStorage.getItem("user")) || [];
+// if (savedUsers.length > 0) {
+//   output1.innerHTML = `<pre>${JSON.stringify(savedUsers, null, 2)}</pre>`;
+// }
 
 //access all items by ids
 const Dform = document.querySelector("form");
 const btn = document.querySelector("#btn");
 const radioInputs = document.querySelectorAll("#gender");
 const small = document.querySelectorAll("small");
+const showOutput = document.querySelector("#output");
+let localstor = document.querySelector("#local-storage");
+// Show last saved user data on page load
+
 //show error on when use r focus on input
 function focusInput() {
   userData.forEach((item) => {
@@ -201,29 +213,35 @@ function input() {
     });
   });
 }
+let users = [];
+let currentIndex = null;
 btn.addEventListener("click", () => {
   validate();
+  localStorage.setItem("user", JSON.stringify(users));
+  JSON.parse(localStorage.getItem("user"));
+  renderUsers();
 });
 function validate() {
-  if (
-    validationStatus.name != true &&
-    validationStatus.user != true &&
-    validationStatus.phone != true &&
-    validationStatus.email != true &&
-    validationStatus.password != true
-  ) {
-    console.log("form can not be submitted");
-  } else {
-    alert("form submitted successfully");
-    Dform.reset();
-    small.forEach((error) => {
-      error.innerHTML = "";
-    });
-    userData.forEach((item) => {
-      document.getElementById(`${item.label}`).style.border = "none";
-    });
-    // focusInput();
-  }
+  // if (
+  //   validationStatus.name != true ||
+  //   validationStatus.user != true ||
+  //   validationStatus.phone != true ||
+  //   validationStatus.email != true ||
+  //   validationStatus.password != true
+  // ) {
+  //   console.log("form can not be submitted");
+  // } else {
+  submitCall();
+
+  Dform.reset();
+  small.forEach((error) => {
+    error.innerHTML = "";
+  });
+  userData.forEach((item) => {
+    const el = document.getElementById(`${item.label}`);
+    if (el) el.style.border = "none";
+  });
+  // }
 }
 document.getElementById("checkbox").addEventListener("change", (e) => {
   if (e.target.checked) {
@@ -232,3 +250,76 @@ document.getElementById("checkbox").addEventListener("change", (e) => {
     document.querySelector(".checkbox").style.visibility = "visible";
   }
 });
+function submitCall() {
+  let user = {};
+
+  userData.forEach((item) => {
+    let inputValue = document.getElementById(item.label);
+    let finalVal = inputValue.value;
+    user[item.label] = finalVal;
+  });
+  if (currentIndex != null) {
+    users[currentIndex] = user;
+    // updateUi();
+    currentIndex = null;
+  } else {
+    users.push(user);
+  }
+  console.log(users);
+  // updateUi();
+  btn.innerHTML = "Submit";
+}
+// function updateUi() {
+//   let output = "";
+//   output += `<table border="4" style="width:100%;">`;
+//   userData.forEach((data) => {
+//     output += `<th style="color:rgb(163, 246, 174)">${data.label}</th>`;
+//   });
+//   output += `<th >Action</th><tbody>`;
+//   users.forEach((user, index) => {
+//     // output += `<div>`;
+//     output += `<tr>`;
+//     userData.forEach((data) => {
+//       output += `<td> ${user[data.label]}</td>`;
+//     });
+//     output += `<td><button onclick="editMe(${index})">edit</button>`;
+//     output += `<button onclick="deleteMe(${index})">delete</button></td>`;
+//   });
+//   output += `</tr><br></tbody></table>`;
+//   btn.innerHTML = "submit";
+
+//   showOutput.innerHTML = output;
+// }
+function renderUsers() {
+  // Load users from localStorage if available
+  const savedUsers = JSON.parse(localStorage.getItem("user")) || [];
+  if (savedUsers.length > 0) {
+    users = savedUsers;
+  }
+  localstor.innerHTML = users
+    .map(
+      (user, index) =>
+        ` ${user.name} , ${user.user}, ${user.phone},
+       ${user.email}, ${user.password}, ${user.websiteURL}
+       <button onclick="editMe(${index})">edit</button>
+       <button onclick="deleteMe(${index})">delete</button><br>`
+    )
+    .join("");
+}
+renderUsers();
+function editMe(index) {
+  currentIndex = index;
+  let user = users[index];
+  userData.forEach((item) => {
+    let input = document.getElementById(`${item.label}`);
+    if (input) {
+      input.value = user[item.label] || "";
+    }
+  });
+  btn.innerHTML = "Update";
+}
+function deleteMe(index) {
+  users.splice(index, 1);
+  localStorage.setItem("user", JSON.stringify(users));
+  renderUsers();
+}
